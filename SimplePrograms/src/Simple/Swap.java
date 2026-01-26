881cf81330222c21d68a4108ed5f250bfc0b134d
Venkatesh <Venkatesh@LAPTOP-DV5EARNU>
2026-01-27 00:36:39 +0530

    Swap 2 numbers without new variable

---
+++ SimplePrograms/src/Simple/Swap.java
@@ -0,0 +1,21 @@
+package Simple;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		a=10;
+		b=20;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		a = a+b;
+		b = a-b;
+		a = a-b;
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		
+
+	}
+
+}
