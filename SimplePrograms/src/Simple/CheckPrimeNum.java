608681b1d44bf115b7b5c3193758f1ba65d7fc55
venkateshwaran <venkateshwaran@192.168.1.8>
2026-01-30 22:33:31 +0530

    New Program Added : Check Prime Number

---
+++ SimplePrograms/src/Simple/CheckPrimeNum.java
@@ -0,0 +1,38 @@
+package Simple;
+// Check number is Prime or not
+import java.util.Scanner;
+
+public class CheckPrimeNum {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static boolean isPrime(int n)
+	{
+		if(n<=1)
+		{
+			return false;
+		}
+		for(int i=2;i<=n/2;i++)
+		{
+			if(n % i == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args)
+	{	
+		System.out.println("Enter the num : ");
+		int n = sc.nextInt();
+		if(isPrime(n))
+		{
+			System.out.println(n+" is a Prime Number!");
+		}
+		else
+		{
+			System.out.println(n+" is not a Prime Number");
+		}
+	}
+
+}
