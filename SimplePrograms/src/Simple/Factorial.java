ec8c30ac145ba1a14c40def095941a46c2ec44a3
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-06 21:59:04 +0530

    New Program Added : Find Factorial

---
+++ SimplePrograms/src/Simple/Factorial.java
@@ -0,0 +1,20 @@
+package Simple;
+
+import java.util.Scanner;
+
+public class Factorial {
+	
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args)
+	{
+		System.out.println("Enter num : ");
+		int f = 1;
+		int n = sc.nextInt();
+		for(int i=n;i>0;i--)
+		{
+			f = f*i;
+		}
+		System.out.println("Factorial of " + n + " = "+f);
+	}
+}
