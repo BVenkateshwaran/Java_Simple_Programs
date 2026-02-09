51b7f41bb05d9e27d78aa99a92bc240ab2572330
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-09 19:26:34 +0530

    New Program Added : Check Armstrong Number

--- SimplePrograms/src/Simple/CheckArmstrongNumber.java
+++ SimplePrograms/src/Simple/CheckArmstrongNumber.java
@@ -14,7 +14,8 @@ public class CheckArmstrongNumber {
 		return d;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Enter a number : ");
 		int n = sc.nextInt();
