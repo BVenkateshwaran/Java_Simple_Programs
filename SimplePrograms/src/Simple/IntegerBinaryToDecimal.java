ba697c23f1faea8ce04937d69b7e3041e8e35a3f
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-12 20:01:09 +0530

    New Program Added : Binary Integer To Decimal

---
+++ SimplePrograms/src/Simple/IntegerBinaryToDecimal.java
@@ -0,0 +1,28 @@
+package Simple;
+import java.util.Scanner;
+public class IntegerBinaryToDecimal {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static int convert(int n)
+	{
+		int d=0;
+		int i=0,r=0;
+		while(n!=0)
+		{
+			r = n%10;
+			n = n/10;
+			d += r * Math.pow(2, i);
+			i++;
+		}
+		return d;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter a binary number : ");
+		int n = sc.nextInt();
+		int d = convert(n);
+		System.out.println("Binary "+n+" = "+d+" Decimal");
+	}
+
+}
