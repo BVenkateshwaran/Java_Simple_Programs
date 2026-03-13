7d453c32e2a4fe753613a5cac43ce0ff642ca8cb
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-13 23:47:31 +0530

    Clockwise Rotation Program Added

---
+++ SimplePrograms/src/Array/RotateClockwise.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RotateClockwise {
+	
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a = {1,5,2,4,3};
+		System.out.println(a[a.length - 1]);
+		System.out.println(Arrays.toString(a));
+		Arrays.sort(a);
+		System.out.println("Sorted Array : "+Arrays.toString(a));
+		System.out.println("Enter the number of times to rotate the array : ");
+		int k = sc.nextInt();
+		for (int i = 0; i < k; i++)
+		{
+			int l = a[a.length - 1];
+			for (int j = a.length-1; j>0; j--)
+			{
+				a[j] = a[j-1];
+			}
+			a[0] = l;
+		}
+		System.out.println(Arrays.toString(a));
+	}
+}
