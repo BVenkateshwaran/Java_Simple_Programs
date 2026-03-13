63c0467bd85e909de5e5ce5dd5adc492125f0817
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-14 00:12:41 +0530

    New Program Added : Array Anti Clockwise Rotation

--- SimplePrograms/src/Array/RotateAntiClockwise.java
+++ SimplePrograms/src/Array/RotateAntiClockwise.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 import java.util.Scanner;
 
-public class RotateAntiClockwise {
-	
+public class RotateAntiClockwise
+{	
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args)
