fe028b7aabfb716d6bd18faabaec926a996b21e3
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-15 14:05:00 +0530

    Anti Clockwise Rotation Program Updated

--- SimplePrograms/src/Array/RotateAntiClockwise.java
+++ SimplePrograms/src/Array/RotateAntiClockwise.java
@@ -11,6 +11,7 @@ public class RotateAntiClockwise
 	public static void main(String[] args)
 	{
 		int [] a = {1,5,3,2,4};
+		
 		Arrays.sort(a);
 		System.out.println(Arrays.toString(a));
 		System.out.println("Enter the number of times to rotate : ");
