e0d1a6e4070ec03aaa4354c3a9813a27edc48cf1
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-15 14:09:19 +0530

    New Package "String" added

---
+++ SimplePrograms/src/String/String_Program.java
@@ -0,0 +1,18 @@
+package String;
+
+public class String_Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "Hello";
+		String s1 = new String("Hii");
+		String s2 = "Hello";
+		System.out.println(s);
+		System.out.println(s1);
+		if(s == s2)
+			System.out.println("Same String");
+		else
+			System.out.println("Not the same String");
+	}
+
+}
