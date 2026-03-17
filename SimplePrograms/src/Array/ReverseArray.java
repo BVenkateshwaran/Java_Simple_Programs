be15e9085a8563235d99daeca0a81e37e3e65cfc
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-18 02:23:59 +0530

    New Array Program : Reverse Array

---
+++ SimplePrograms/src/Array/ReverseArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Enter size : ");
+		int n = sc.nextInt();
+		int [] a = new int [n];
+		//System.out.println(a.length);
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter element a["+i+"] : ");
+			a[i] = sc.nextInt();
+		}
+		for(int i=a.length-1;i>=0;i--)
+		{
+			System.out.println(a[i]);
+		}
+	}
+
+}
