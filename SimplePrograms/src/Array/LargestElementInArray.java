6b4bcd2dab5be34e70d01c4d74325b11c1a22d90
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-19 00:03:14 +0530

    New Array Program : Largest Element in Array

---
+++ SimplePrograms/src/Array/LargestElementInArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class LargestElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a [] = {1,8,4,10,6,2,11,7,9,12,6,3,5};
+		int max = Integer.MIN_VALUE;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i] > max)
+			{
+				max = a[i];
+			}
+		}
+		System.out.println("Largest Element in Array : "+max);
+	}
+
+}
