328ecf71d3e2ba9446c4916f75734b78568dce39
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-19 01:30:19 +0530

    Anti Clockwise Rotation in Array

--- SimplePrograms/src/Array/RotateAntiClockwise.java
+++ SimplePrograms/src/Array/RotateAntiClockwise.java
@@ -10,7 +10,7 @@ public class RotateAntiClockwise
 	
 	public static void main(String[] args)
 	{
-		int [] a = {1,5,3,2,4};
+		int [] a = {1,2,3,4,5};
 		
 		Arrays.sort(a);
 		System.out.println(Arrays.toString(a));
