33a21ad90a430e608b1359a271ce048039f4f186
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-21 22:16:27 +0530

    Merge Sort Updated

--- SimplePrograms/src/Array/MergeSort.java
+++ SimplePrograms/src/Array/MergeSort.java
@@ -17,41 +17,31 @@ public class MergeSort
 	
 	public static void merge(int [] a,int l, int m, int h)
 	{
-		int left=l,right=m+1;
-		int x=0;
+		int left = l,right = m+1;
 		int [] k = new int[a.length];
-		while(left<=m && right<=h)
+		int x = 0;
+		while(left <= m && right <= h)
 		{
 			if(a[left] < a[right])
-			{
 				k[x++] = a[left++];
-			}
 			else if(a[left] > a[right])
-			{
 				k[x++] = a[right++];
-			}
 			else
-			{
-				k[x] = a[right];
-				x++;
-				right++;
-				left++;
-			}
+				k[x++] = a[left++];
 		}
-		while(left<=m)
+		while(left <= m)
 			k[x++] = a[left++];
-		while(right<=h)
+		while(right <= h)
 			k[x++] = a[right++];
-		
-		//System.out.println("K : "+Arrays.toString(k));
-		
+		//System.out.println("K in loop : "+Arrays.toString(k));
 		x=0;
 		for(int i=l;i<=h;i++)
 			a[i] = k[x++];
+		//System.out.println("A in loop : "+Arrays.toString(a));
 	}
 	
 	public static void main(String[] args) {
-		int [] a = {1,9,2,8,3,7,4,6,5};
+		int [] a = {1,9,2,8,3,10,7,4,6,5};
 		System.out.println("A : "+Arrays.toString(a));
 		int l=0,h=a.length-1;
 		divide(a,l,h);
