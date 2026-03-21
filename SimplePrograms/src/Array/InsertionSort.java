2ba7304d5e220c4efb028ae94d9d64a2eb9098d5
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-21 21:34:32 +0530

    Insertion Sort Updated

--- SimplePrograms/src/Array/InsertionSort.java
+++ SimplePrograms/src/Array/InsertionSort.java
@@ -5,20 +5,21 @@ public class InsertionSort
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args)
 	{
-		int [] a = {10,9,2,8,3,7,4,6,5};
+		int [] a = {10,9,2,8,3,7,4,6,5,1};
 		System.out.println("A : "+Arrays.toString(a));
 		for(int i=1;i<a.length;i++)
 		{
 			int r = a[i];
-			int l=i-1;
+			int l = i-1;
 			while(l>=0 && a[l]>r)
 			{
 				a[l+1] = a[l];
 				l--;
+				//System.out.println("A in while-loop where i="+i+" : "+Arrays.toString(a));
 			}
 			a[l+1] = r;
-			System.out.println("A : "+Arrays.toString(a));
+			//System.out.println("A in for-loop where i="+i+" :  : "+Arrays.toString(a));
 		}
-		System.out.println("A : "+Arrays.toString(a));
+		System.out.println("A :"+Arrays.toString(a));
 	}
 }
