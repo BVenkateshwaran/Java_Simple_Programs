4e4f09e35851d54a00c3a354d6b3f7f16e8b1a40
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-22 13:39:26 +0530

    Converting Char Array to String Program

---
+++ SimplePrograms/src/String/CharArray_to_String.java
@@ -0,0 +1,10 @@
+package String;
+import java.util.*;
+public class CharArray_to_String {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		char [] a = {'h','e','l','l','o'};
+		String s = new String(a);
+		System.out.println("S : "+s);
+	}
+}
