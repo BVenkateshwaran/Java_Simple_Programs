20eb1c43ffe1ac87d2bccf024e0286729dabe6ac
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-03-22 13:12:41 +0530

    Convert String to Char Array

---
+++ SimplePrograms/src/String/String_to_CharArray.java
@@ -0,0 +1,11 @@
+package String;
+import java.util.*;
+public class String_to_CharArray {
+	static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		String s = "hello";
+		char [] a = s.toCharArray();
+		System.out.println(s);
+		System.out.println(a[0]);
+	}
+}
