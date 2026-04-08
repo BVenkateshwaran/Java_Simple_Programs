08c3673f5c2c7a78a286df40b6b810cfcc3aa139
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-04-08 21:10:09 +0530

    Check String in Java

--- SimplePrograms/src/String/String_Program.java
+++ SimplePrograms/src/String/String_Program.java
@@ -5,7 +5,6 @@ public class String_Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s = "Hello";
-		
 		String s1 = new String("Hii");
 		String s2 = "Hello";
 		
