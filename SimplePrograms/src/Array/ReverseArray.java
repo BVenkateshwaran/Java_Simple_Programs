45f22a5564e249f8d15f3ed1e93b5ab27f9eb309
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-04-15 17:26:34 +0530

    Reverse elements present in an Array

--- SimplePrograms/src/Array/ReverseArray.java
+++ SimplePrograms/src/Array/ReverseArray.java
@@ -6,7 +6,8 @@ public class ReverseArray
 {
 	static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Enter size : ");
 		int n = sc.nextInt();
