c67ec3756f013c9b83d3a32606f9828518a93032
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-04-21 19:37:11 +0530

    The Sum of Prime Numbers in Array

--- SimplePrograms/src/Array/SumOfPrimesInArray.java
+++ SimplePrograms/src/Array/SumOfPrimesInArray.java
@@ -1,11 +1,8 @@
 package Array;
-
 import java.util.Scanner;
-
 public class SumOfPrimesInArray
 {	
 	static Scanner sc = new Scanner(System.in);
-	
 	public static boolean check(int n)
 	{
 		if(n<=1)
@@ -24,11 +21,11 @@ public class SumOfPrimesInArray
 		}
 		return true;
 	}
-	
 	public static void main(String[] args)
 	{
 		int [] a = {4,5,2,6,8,7,11};
 		int sum=0;
+		
 		for(int i=0;i<a.length;i++)
 		{
 			if(check(a[i]))
