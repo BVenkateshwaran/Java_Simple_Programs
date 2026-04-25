b345b34a7dc55e082ca6a0f06e007e0f85b10f1b
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-04-25 19:33:40 +0530

    Reversing Array

--- SimplePrograms/src/Array/ReverseArray.java
+++ SimplePrograms/src/Array/ReverseArray.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class ReverseArray
 {
 	static Scanner sc = new Scanner(System.in);
-	
 	public static void main(String[] args)
 	{
 		System.out.println("Enter size : ");
 		int n = sc.nextInt();
-		
 		int [] a = new int [n];
-		
 		//System.out.println(a.length);
 		for(int i=0;i<a.length;i++)
 		{
-			System.out.println("Enter element a["+i+"] : ");
+			System.out.println("Enter element a["+i+"]:");
 			a[i] = sc.nextInt();
 		}
 		for(int i=a.length-1;i>=0;i--)
@@ -24,5 +21,4 @@ public class ReverseArray
 			System.out.println(a[i]);
 		}
 	}
-
 }
