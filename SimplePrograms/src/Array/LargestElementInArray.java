67191439c86be55fb3d8b9130ba8ac71b2bc5507
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-05-03 19:41:29 +0530

    The largest element in an array

--- SimplePrograms/src/Array/LargestElementInArray.java
+++ SimplePrograms/src/Array/LargestElementInArray.java
@@ -4,6 +4,7 @@ public class LargestElementInArray
 	public static void main(String[] args)
 	{
 		int a [] = {1,8,4,10,6,2,11,7,9,12,6,3,5};
+		
 		int max = Integer.MIN_VALUE;
 		
 		for(int i=0;i<a.length;i++)
