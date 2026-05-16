77dd61a795eef16cc157e5c7b4f8e399209a3fc1
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-05-16 20:17:26 +0530

    LeetCode - Sunlight on Buildings

--- SimplePrograms/src/Array/LeetCode_BuildingSunLight.java
+++ SimplePrograms/src/Array/LeetCode_BuildingSunLight.java
@@ -4,7 +4,7 @@ public class LeetCode_BuildingSunLight
 {
 	public static void main(String[] args)
 	{
-		int a[] = {4,2,5,3,6,5,3};
+		int a [] = {4,2,5,3,6,5,3};
 		
 		int max = Integer.MIN_VALUE,count = 0;
 		for(int i=0; i<a.length;i++)
