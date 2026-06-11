5d289a5f6fcc152d27afebdb57ea9403b999c747
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-06-11 18:11:10 +0530

    leap year in java

--- SimplePrograms/src/Simple/LeapYear.java
+++ SimplePrograms/src/Simple/LeapYear.java
@@ -10,6 +10,7 @@ public class LeapYear
 	public static void main(String[] args)
 	{	
 		System.out.println("Enter year : ");
+		
 		int year = sc.nextInt();
 		if ( ( year %4 == 0 && year % 100 != 0 ) || (year % 400 == 0) )
 		{
