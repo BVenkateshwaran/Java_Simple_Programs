36b70a2d4e5a9641061228f789e89de1967e7535
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-07-25 11:34:28 +0530

    Happy Number in Java

--- SimplePrograms/src/Simple/HappyNumber.java
+++ SimplePrograms/src/Simple/HappyNumber.java
@@ -6,7 +6,8 @@ public class HappyNumber
 {
 	static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Enter number to check : ");
 		int n = sc.nextInt();
