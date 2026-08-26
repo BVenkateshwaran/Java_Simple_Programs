9b8aac1c51c3e4d42b0f598ed3278ef574dc50a7
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-08-26 21:27:27 +0530

    Fah to Cel In Java

--- SimplePrograms/src/Simple/Fah_to_Cel.java
+++ SimplePrograms/src/Simple/Fah_to_Cel.java
@@ -6,7 +6,8 @@ public class Fah_to_Cel
 {
 	static Scanner sc = new Scanner(System.in);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Enter temperature in Fahrenheit : ");
 		double fah = sc.nextDouble();
