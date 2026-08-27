445757e694c446cfc7da05135d9ff6ec8c64bcae
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-08-28 00:01:17 +0530

    Fahrenheit to Celcius

--- SimplePrograms/src/Simple/Fah_to_Cel.java
+++ SimplePrograms/src/Simple/Fah_to_Cel.java
@@ -11,7 +11,7 @@ public class Fah_to_Cel
 		// TODO Auto-generated method stub
 		System.out.println("Enter temperature in Fahrenheit : ");
 		double fah = sc.nextDouble();
-		double cel = (fah -32) * (5.0/9.0);
+		double cel = (fah - 32) * (5.0/9.0);
 		System.out.println(fah+" Fahrenheit = "+cel+" Celsius");
 	}
 
